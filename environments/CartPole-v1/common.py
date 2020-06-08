5849ac7fd041b1a0bb17449c4868e6e918524678
Archit Rungta <architrungta120@gmail.com>
2020-06-08 20:01:23 +0530

    Create common.py

---
+++ environments/CartPole-v1/common.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2018 Archit Rungta
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
+import os
+
+import click
+import gym
+import neat
+import numpy as np
+from pytorch_neat.discount_factor_eval import DiscountEnvEvaluator
+from pytorch_neat.standardise_eval import StandardEnvEvaluator
+from pytorch_neat.multi_env_eval import MultiEnvEvaluator
+from pytorch_neat.neat_reporter import TensorBoardReporter
+from pytorch_neat.recurrent_net import RecurrentNet
+from pytorch_neat.rewardtogo_eval import RewardToGoEnvEvaluator
+
+max_env_steps = 200
+
+env_name = "CartPole-v1"
+
+def make_env():
+    return gym.make(env_name)
+
+
+def make_net(genome, config, bs):
+    return RecurrentNet.create(genome, config, bs)
+
+
+def activate_net(net, states):
+    output = net.activate(states).numpy()
+    return output[:, 0]>0.5
